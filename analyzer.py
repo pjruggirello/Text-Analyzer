e100af21e07c39b4774e603dfe790f4184dda144
Philip Ruggirello <pjruggirello@gmail.com>
2021-01-12 16:41:17 -0500

    Added lists of types of words (nounds, verbs, etc.) as well as prompt to select file to be analyzed.

--- analyzer.py
+++ analyzer.py
@@ -1,21 +1,28 @@
 # Philip Ruggirello 1/12/21
 
 class Analyzer:
-	"""This will be the analyzer onbject whihc will contains the mehtods for the different ways the inputed txt file can be anaylzed."""
+	"""This will be the analyzer onbject whihc will contains the mehtods for the different ways the inputed txt file can be analyzed."""
 	def __init__(self):
 		"""Conatins the attributes of the analyzer object."""
-		self.nouns = '/home/peejay/python/text_analysis/nouns.txt'
-		self.pronouns = ['i', 'me', 'my', 'mine', 'myself',
-					     'you', 'your', 'yours', 'yourself',
-					     'he', 'him', 'his', 'himself',
-					     'she', 'her', 'hers', 'herself',
-					     'it', 'its', 'itself',
-					     'we', 'us', 'our', 'ours', 'ourselves', 
-					     'yourselves',
-					     'they', 'them', 'their', 'theirs', 'themselves']
-		self.verbs = '/home/peejay/python/text_analysis/verbs.txt'
-		self.adjectives = '/home/peejay/python/text_analysis/adjectives.txt'
-		self.adverbs = '/home/peejay/python/text_analysis/adverbs.txt'
-		self.prepositions = []
-		self.conjunctions = []
-		self.interjections = []
+		self.list_of_nouns = '/home/peejay/python/text_analysis/nouns.txt'
+		self.list_of_pronouns = '/home/peejay/python/text_analysis/pronouns.txt'
+		self.list_of_verbs = '/home/peejay/python/text_analysis/verbs.txt'
+		self.list_of_adjectives = '/home/peejay/python/text_analysis/adjectives.txt'
+		self.list_of_adverbs = '/home/peejay/python/text_analysis/adverbs.txt'
+		self.list_of_prepositions = '/home/peejay/python/text_analysis/prepositions.txt'
+		self.list_of_conjunctions = '/home/peejay/python/text_analysis/conjunctions.txt'
+		self.list_of_interjections = '/home/peejay/python/text_analysis/interjections.txt'
+
+	def select_file(self):
+		"""Greets the user and ask's for them to input the file path of the file that they wish to analyze."""
+		self.text_file = input("Please enter the path of the text file you would like to have analyzed: ")
+
+		return self.text_file
+
+	def nouns(self):
+		"""Tells the user how many nouns are in their text file."""
+		nouns = 0
+		with (self.text_file) open as f:
+			f.readline
+		for noun in self.list_of_nouns:
+			if noun in 
