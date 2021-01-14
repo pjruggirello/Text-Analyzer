24001735675712365f5bda9c3b0634aeb686186c
Philip Ruggirello <pjruggirello@gmail.com>
2021-01-13 19:24:43 -0500

    Program is able to print all the nouns in the user input file as well as how many time each were used. Now need to put counter into its own method and repeat for the other types of words.

--- analyzer.py
+++ analyzer.py
@@ -1,17 +1,19 @@
 # Philip Ruggirello 1/12/21
 
+from collections import Counter
+
 class Analyzer:
 	"""This will be the analyzer onbject whihc will contains the mehtods for the different ways the inputed txt file can be analyzed."""
 	def __init__(self):
 		"""Conatins the attributes of the analyzer object."""
-		self.list_of_nouns = '/home/peejay/python/text_analysis/nouns.txt'
-		self.list_of_pronouns = '/home/peejay/python/text_analysis/pronouns.txt'
-		self.list_of_verbs = '/home/peejay/python/text_analysis/verbs.txt'
-		self.list_of_adjectives = '/home/peejay/python/text_analysis/adjectives.txt'
-		self.list_of_adverbs = '/home/peejay/python/text_analysis/adverbs.txt'
-		self.list_of_prepositions = '/home/peejay/python/text_analysis/prepositions.txt'
-		self.list_of_conjunctions = '/home/peejay/python/text_analysis/conjunctions.txt'
-		self.list_of_interjections = '/home/peejay/python/text_analysis/interjections.txt'
+		self.list_of_nouns = '/home/peejay/python/text_analysis/dictionaries/nouns.txt'
+		self.list_of_pronouns = '/home/peejay/python/text_analysis/dictionaries/pronouns.txt'
+		self.list_of_verbs = '/home/peejay/python/text_analysis/dictionaries/verbs.txt'
+		self.list_of_adjectives = '/home/peejay/python/text_analysis/dictionaries/adjectives.txt'
+		self.list_of_adverbs = '/home/peejay/python/text_analysis/dictionaries/adverbs.txt'
+		self.list_of_prepositions = '/home/peejay/python/text_analysis/dictionaries/prepositions.txt'
+		self.list_of_conjunctions = '/home/peejay/python/text_analysis/dictionaries/conjunctions.txt'
+		self.list_of_interjections = '/home/peejay/python/text_analysis/dictionaries/interjections.txt'
 
 	def select_file(self):
 		"""Greets the user and ask's for them to input the file path of the file that they wish to analyze."""
@@ -19,10 +21,30 @@ class Analyzer:
 
 		return self.text_file
 
-	def nouns(self):
+	def how_many_nouns(self):
 		"""Tells the user how many nouns are in their text file."""
+		words_from_story = []
+		nouns_in_list = []
 		nouns = 0
-		with (self.text_file) open as f:
-			f.readline
-		for noun in self.list_of_nouns:
-			if noun in 
+		contained = []
+		with open(self.text_file) as f:
+			for line_of_book in f:
+				for word in line_of_book.split():
+					words_from_story.append(word.strip())
+		with open(self.list_of_nouns) as n:
+			for line_of_list in n:
+				for noun in line_of_list.split():
+					nouns_in_list.append(noun.strip())
+
+		c1 = Counter(words_from_story)
+		c2 = Counter(nouns_in_list)
+
+		intersections = { k: c1[k] * c2[k] for k in c1.keys() & c2.keys() }
+
+		for key, value in intersections.items():
+			print(f"{key}: {value}")
+
+if __name__ == '__main__':
+	a = Analyzer()
+	a.select_file()
+	a.how_many_nouns()
